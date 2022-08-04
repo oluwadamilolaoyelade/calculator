e580ae4798beda0cff78babd19a2ec4050f8136b
oluwadamilolaoyelade <107127029+oluwadamilolaoyelade@users.noreply.github.com>
2022-08-04 16:41:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// let user;
+// console.log(typeof user);
+// let userName="Tobi"
+// console.log(typeof userName);
+// let sample="word"/5
+// console.log(sample)
+
+// let toString=String(345)
+// console.log(typeof toString)
+// let toNumber=Number('word')
+// console.log(typeof toNumber)
+// const arr = [2,3,4]
+// console.log(arr)
+// alert(location.href); // shows current URL
+// if (confirm("Go to Wikipedia?")) {
+//   location.href = "https://wikipedia.org"; // redirect the browser to another URL
+// }
+
+// for (let node of document.body.childNodes) {
+//     console.log(node); // shows all nodes from the collection
+//   }
+  
+let div = document.createElement('div');
+div.className = 'first'
+div.innerHTML = '<p>This is Dayo</p>'
+document.body.appendChild(div)
