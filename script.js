a99d173e8efd0624ec8d02647a28b4beac17b6c2
oluwadamilolaoyelade <107127029+oluwadamilolaoyelade@users.noreply.github.com>
2022-08-12 12:24:59 +0100

    first commit

--- script.js
+++ script.js
@@ -1,26 +1,128 @@
-// let user;
-// console.log(typeof user);
-// let userName="Tobi"
-// console.log(typeof userName);
-// let sample="word"/5
-// console.log(sample)
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+        // this.appendNumber()
+        // this.updateDisplay()
+    }
 
-// let toString=String(345)
-// console.log(typeof toString)
-// let toNumber=Number('word')
-// console.log(typeof toNumber)
-// const arr = [2,3,4]
-// console.log(arr)
-// alert(location.href); // shows current URL
-// if (confirm("Go to Wikipedia?")) {
-//   location.href = "https://wikipedia.org"; // redirect the browser to another URL
-// }
+    clear() {
+       this.currentOperand =  ''
+       this.previousOperand =  ''
+       this.operation = undefined
+    }
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== ''){
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case 'x':
+                computation = prev * current
+                break
+            case '/':
+                computation = prev / current
+                break
+            case '%':
+                computation = prev / 100
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+            maximumFractionDigits: 0})
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        }   else {
+            return integerDisplay
+        }
+        // const floatNumber = parseFloat(number)
+        // if (isNaN(floatNumber)) return ''
+        // return floatNumber.toLocaleString('en')
+    }
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = 
+        this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null){
+            this.previousOperandTextElement.innerText = 
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
+    }
+}
 
-// for (let node of document.body.childNodes) {
-//     console.log(node); // shows all nodes from the collection
-//   }
-  
-let div = document.createElement('div');
-div.className = 'first'
-div.innerHTML = '<p>This is Dayo</p>'
-document.body.appendChild(div)
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+})
+})
+
+equalsButton.addEventListener('click', buttton =>{
+    calculator.compute()
+    calculator.updateDisplay()
+})
+allClearButton.addEventListener('click', buttton =>{
+    calculator.clear()
+    calculator.updateDisplay()
+})
+deleteButton.addEventListener('click', buttton =>{
+    calculator.delete()
+    calculator.updateDisplay()
+})
